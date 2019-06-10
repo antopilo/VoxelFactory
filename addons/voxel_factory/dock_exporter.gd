cfa6fd458f00cbca5d8b4e0b2bc099273a333aa9
unknown <antoinepilote1@hotmail.com>
2019-06-10 19:20:06 -0400

    Started plugin panels

---
+++ addons/voxel_factory/dock_exporter.gd
@@ -0,0 +1,50 @@
+tool
+extends Control 
+
+var fs = null
+var current_file = null
+onready var preview = get_node("Panel/VBoxContainer/MarginContainer2/TextureRect")
+onready var openButton = get_node("Panel/VBoxContainer/MarginContainer/Button")
+onready var exportButton = get_node("Panel/VBoxContainer/MarginContainer3/Button")
+onready var fileDialog = get_node("FileDialog")
+onready var exportDialog = get_node("SaveDialog")
+onready var voxelFactory = get_node("VoxelFactory")
+
+# Load button
+func _on_OpenButton_Pressed():
+	fileDialog.popup_centered_minsize()
+
+# C# call
+func exportMesh(dir):
+	var mesh = voxelFactory.create_mesh_from_image_file(current_file)
+	print("saved at: " + dir)
+	for vox in voxelFactory.Voxels:
+		mesh.set_meta(str(vox), voxelFactory.Voxels[vox])
+		
+	ResourceSaver.save(dir, mesh)
+	
+# Load Dialog
+func _on_FileDialog_file_selected(path):
+	current_file = path
+	updatePreview()
+
+# Update the preview window
+func updatePreview():
+	var image = Image.new() 
+	image.load(current_file)
+	
+	var tex = ImageTexture.new()
+	tex.create_from_image(image,0)
+	tex.flags = 0
+	
+	preview.texture = tex
+	
+# Export Button
+func _on_Button_pressed():
+	exportDialog.popup_centered_minsize()
+	if fs != null:
+		print('Scanned file system.')
+		fs.scan()
+		
+func _on_SaveDialog_file_selected(path):
+	exportMesh(path)
--- addons/voxel_factory/plugin_factory.gd
+++ addons/voxel_factory/plugin_factory.gd
@@ -1,12 +1,22 @@
 tool
 extends EditorPlugin
 
+var dock
+var editor
 func _enter_tree():
     # Initialization of the plugin goes here
     # Add the new type with a name, a parent type, a script and an icon
-    add_custom_type("VoxelFactory", "Spatial", preload("voxel_factory.gd"), preload("icon.png"))
-
+	add_custom_type("VoxelFactory", "Spatial", preload("voxel_factory.gd"), preload("icon.png"))
+	dock = preload("res://addons/voxel_factory/dock_exporter.tscn").instance()
+	dock.fs = get_editor_interface().get_resource_filesystem()
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_UR, dock)
+	
+	editor = preload("res://addons/voxel_factory/voxel_editor.tscn").instance()
+	add_control_to_bottom_panel(editor, "Voxel editor")
+	
 func _exit_tree():
     # Clean-up of the plugin goes here
     # Always remember to remove it from the engine when deactivated
-    remove_custom_type("voxel_factory")
+	remove_custom_type("voxel_factory")
+	remove_control_from_docks(dock)
+	remove_control_from_bottom_panel(editor)
